e8e3f0cf5f06159322cd1cb7b52e8b5d3cec8301
Manujademin <137299721+Manujademin@users.noreply.github.com>
2023-07-22 03:35:47 -0700

    Add files via upload

---
+++ coffemakerbrista.py
@@ -0,0 +1,23 @@
+#lets make a robot brista to work at a Resturent
+
+print("Heloo coustomer welcome to our coffe shop")
+ 
+name = input("what is you're name ")
+
+print(name +" we'll bring you a menu soon as posible ")
+
+print("--------menu-------------")
+print("Black Coffe")
+print("white coffe")
+print("cold coffe")
+print("Espresso")
+
+print("what would you like to order? ")
+
+order = input("May I take youre order? ")
+
+print("Hey "+ name +" youre order will be ready in a moment...")
+
+
+
+
